9c01e1d2ea744f3b3b61e99e03fb10dc0432bf36
Chaoyi He <chaoyi_he@tamu.edu>
2024-01-07 14:09:57 -0600

    modified:   cfg/cfg.yaml 	modified:   main.py 	modified:   models/detection.py

--- models/detection.py
+++ models/detection.py
@@ -40,7 +40,7 @@ class MLP(nn.Module):
 
     def forward(self, x):
         for i, layer in enumerate(self.layers):
-            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+            x = F.sigmoid(layer(x)) if i < self.num_layers - 1 else layer(x)
         return x
     
 
